8fd2ebb0db3fe82d2301ae5addc8799b390ce30e
Subhabrata <70436487+TypicalGamer07@users.noreply.github.com>
2023-03-26 18:28:47 +0530

    Q5 a

---
+++ December 2021/concatinate 2 strings.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main () {
+
+    char str1[100], str2[100];
+    int i=0 , j=0;
+
+    printf("Enter first string: \n");
+    gets(str1);
+    printf("Enter second string: \n");
+    gets(str2);
+
+    while(str1[i] !='\0') {
+        i++;
+    }
+    while(str2[j] != '\0') {
+        str1[i]=str2[j];
+        i++;
+        j++;
+    }
+    str1[i]='\0';
+
+    printf("The concatinated string is: %s \n", str1);
+    system("pause");
+    return 0;
+}
