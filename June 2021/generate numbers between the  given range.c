75082e040bcbb582d58d091cf705f6610f9136cc
Subhabrata <70436487+TypicalGamer07@users.noreply.github.com>
2023-03-27 17:59:18 +0530

    Q1 c

---
+++ June 2021/generate numbers between the  given range.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int isodd(int n) {
+    if (n%2 == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+int main() {
+    int start,end,n,i;
+    printf("Enter the starting number: ");
+    scanf("%d", &start);
+    printf("Enter the ending number: ");
+    scanf("%d", &end);
+    printf("The Even number range for %d to %d is: \n",start,end);
+        for(i=start;i<=end;i++) {
+            if(isodd(i)) {
+                printf("%d\n",i);    
+        }
+    }
+    printf("The odd number range for %d to %d is: \n",start,end);
+        for(i=start;i<=end;i++) {
+            if(!isodd(i)) {
+                printf("%d\n",i);    
+            }
+        }
+    system("pause");
+    return 0;
+}
