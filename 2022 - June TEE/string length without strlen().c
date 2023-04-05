71485f28034d8e188502a111b845d544985ccb8e
Subhabrata <70436487+Ironfist69@users.noreply.github.com>
2023-04-05 22:24:29 +0530

    updated strlen()

--- 2022 - June TEE/string length without strlen().c
+++ 2022 - June TEE/string length without strlen().c
@@ -1,17 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 int main () {
-    char string [100];
-    int length=0;
-    printf ("Enter your string: ");
-    scanf ("%s", &string);
-
-    while (string[length] != '\0')
-    {
-        length++;                //while loop to iterate through the str array and count the number of 
-                                  //characters until we encounter the null character \0, which indicates the end of the string
+    int i,length;
+    char str[100];
+    printf("Enter your string: ");
+    scanf("%s", str);
+    for(i=0;str[i] !='\0';i++) {
+        length ++;
     }
-
-    printf("The length of the string is %d \n", length);
+    printf("String length: %d \n", length);
     system("pause");
     return 0;
 }
