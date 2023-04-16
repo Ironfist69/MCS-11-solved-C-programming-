c8d06bbbaadc36b7d0c6a4e09f1a20b8cc2e2b28
Subhabrata <70436487+Ironfist69@users.noreply.github.com>
2023-04-16 08:19:01 +0530

    Q4 b

---
+++ 2020 - December TEE/Write a C program to display the following pattern using “for” loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () {
+    int i,j;
+    for(i=5;i>=0;i--) {
+        for(j=5;j>i;j--) {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    system("pause");
+    return 0;
+}
