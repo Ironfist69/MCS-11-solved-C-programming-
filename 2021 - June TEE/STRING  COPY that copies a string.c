7de7a7b0ada4793022cd01d58c13edf038fa64bd
Ironfist69 <subhabratasen1999@gmail.com>
2023-05-09 14:19:31 +0530

    added linux support (cross platform)

--- 2021 - June TEE/STRING  COPY that copies a string.c
+++ 2021 - June TEE/STRING  COPY that copies a string.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 void copystring(char *str1,char *str2) {
-    int i=0;
-    while(str1[i] !='\0') {
-        str1[i]=str2[i];
-        i++;
+    int i;
+    for (i=0;str1[i]!='\0';i++)
+    {
+        str2[i]=str1[i];
     }
     str2[i] ='\0';
 }
--- 2021 - June TEE/Write a function to print the sum of the following where ‘‘n’’ is passed as an argument to the function.c
+++ 2021 - June TEE/Write a function to print the sum of the following where ‘‘n’’ is passed as an argument to the function.c
@@ -1,26 +1,19 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
 
-void print_series_sum(int n);
-
-int main() {
-    int n;
-    
-    printf("Enter a positive integer n: ");
-    scanf("%d", &n);
-    
-    print_series_sum(n);
-    system("pause");
-    return 0;
-}
-
-void print_series_sum(int n) {
+int seriesSum(int n) {
     int i;
-    double sum = 0.0;
-    
-    for (i = 1; i <= n; i++) {
-        sum += pow(i, i);
+    int sum=0;
+    for(i=1;i<=n;i++) {
+        sum+=pow(i,i);
     }
-    
-    printf("The sum of the series 1^1 + 2^2 + ... + %d^%d is %f\n", n, n, sum);
+    printf("Sum of series: %d \n",sum);
+    return sum;
+}
+int main () {
+    int n;
+    printf("Enter number of terms: ");
+    scanf("%d",&n);
+    seriesSum(n);
+    return 0;
 }
